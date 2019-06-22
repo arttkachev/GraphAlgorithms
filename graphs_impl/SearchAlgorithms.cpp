505eca43475b352ad6c19d67d3b89624cfdb4fa5
arttkachev <art.tkachev@bk.ru>
2019-06-23 00:57:46 +0300

    Dijkstra Implementation to find a shortest path by weights

--- graphs_impl/SearchAlgorithms.cpp
+++ graphs_impl/SearchAlgorithms.cpp
@@ -211,3 +211,141 @@ std::vector<Vertex<T>*>* SearchAlgorithms<T>::depthFirstSearchWithGoal(Graph<T>*
 	// no path found
 	return nullptr;	
 }
+
+template<typename T>
+void SearchAlgorithms<T>::Dijkstra(Graph<T>* graph, Vertex<T>* sourceVertex)
+{
+	// vector of all vertices in the grpah that the algorithm needs to explore
+	std::vector<Vertex<T>*> unfinishedVertices;
+	
+	// explore vertices and add them to the unfinished vector
+	for (Vertex<T>* vertex : graph->getVerticies())
+	{
+		vertex->setParentVertex(nullptr);
+		vertex->setDistance(std::numeric_limits<float>::max()); // imitates the infinity distance to other vertices (like Dijkstra works)
+		unfinishedVertices.push_back(vertex);
+	}
+
+	// in Dijkstra the distance to the source vertex is always set to min. Means 0
+	sourceVertex->setDistance(0.f);
+
+	// main loop of the algorithm (the same pattern like in BFS and DFS)
+	// while all vertices are not explored Dijkstra searches for a shortest path
+	while (unfinishedVertices.size() > 0)
+	{
+		// find the closest vertex
+		Vertex<T>* closestVertex = getClosestVertex(unfinishedVertices);
+
+		// and remove it from unfinishedVertices because now it is finished
+		// to remove a specific element from vector, first we need to find its index or pass in an iterator
+		auto closestVertex_iter = std::find(unfinishedVertices.begin(), unfinishedVertices.end(), closestVertex);
+
+		if (closestVertex_iter != unfinishedVertices.end())
+		{
+			unfinishedVertices.erase(closestVertex_iter);
+		}
+
+
+		// find adjacentVertices of the closest vertex to explore weights to them to find the shortest one by the weights
+		// this is the heart of Dijkstra where we update the actual path to get the shortest one
+		for (Vertex<T>* adjVertex : graph->getAdjacentVertices(closestVertex))
+		{
+			if (adjVertex->getDistance() > closestVertex->getDistance() + graph->getEdgeWeight(closestVertex, adjVertex))
+			{
+				// if true it means the algorithm found the shortest path. We update the distance of the current adjacent Vertex
+				adjVertex->setDistance(closestVertex->getDistance() + graph->getEdgeWeight(closestVertex, adjVertex));
+
+				// and we store closestVertex as the parent of found adjacent vertex to form the actual path
+				adjVertex->setParent(closestVertex);
+			}
+		}
+	}
+}
+
+template<typename T>
+std::vector<Vertex<T>*> SearchAlgorithms<T>::DijkstraWithGoal(Graph<T>* graph, Vertex<T>* sourceVertex, Vertex<T>* goalVertex)
+
+{
+	// just check if the algorithm has found the goal
+	if (sourceVertex == goalVertex)
+	{		
+		return std::vector<Vertex<T>*> { sourceVertex };
+	}
+
+	// vector of all vertices in the grpah that the algorithm needs to explore
+	std::vector<Vertex<T>*> unfinishedVertices;
+
+	// explore vertices and add them to the unfinished vector
+	for (Vertex<T>* vertex : graph->getVerticies())
+	{		
+		vertex->setParentVertex(nullptr);
+		vertex->setDistance(std::numeric_limits<float>::max()); // imitates the infinity distance to other vertices (like Dijkstra works)
+		unfinishedVertices.push_back(vertex);
+	}
+
+	// in Dijkstra the distance to the source vertex is always set to min. Means 0
+	sourceVertex->setDistance(0.f);
+
+	// main loop of the algorithm (the same pattern like in BFS and DFS)
+	// while all vertices are not explored Dijkstra searches for a shortest path
+	while (unfinishedVertices.size() > 0)
+	{
+		// find the closest vertex		
+		Vertex<T>* closestVertex = getClosestVertex(unfinishedVertices);
+
+		// and remove it from unfinishedVertices because now it is finished
+		// to remove a specific element from vector, first we need to find its index or pass in an iterator
+		auto closestVertex_iter = std::find(unfinishedVertices.begin(), unfinishedVertices.end(), closestVertex);
+
+		if (closestVertex_iter != unfinishedVertices.end())
+		{			
+			unfinishedVertices.erase(closestVertex_iter);
+		}	
+
+		// find again if the the closest vertex is a goal. 
+		// If so, since we have removed it from unfinishedVertices, so, we know we have already updated all the shortest paths to it
+		// and just return the path to the closest vertex because the shortest path has already been found
+		if (closestVertex == goalVertex)
+		{			
+			return *getPathToSource(closestVertex);
+		}
+		
+
+		// otherwise, just continue as before
+
+		// find adjacentVertices of the closest vertex to explore weights to them to find the shortest one by the weights
+		// this is the heart of Dijkstra where we update the actual path to get the shortest one
+		for (Vertex<T>* adjVertex : graph->getAdjacentVertices(closestVertex))
+		{
+			if (adjVertex->getDistance() > closestVertex->getDistance() + graph->getEdgeWeight(closestVertex, adjVertex))
+			{
+				// if true it means the algorithm found the shortest path. We update the distance of the current adjacent Vertex
+				adjVertex->setDistance(closestVertex->getDistance() + graph->getEdgeWeight(closestVertex, adjVertex));
+
+				// and we store closestVertex as the parent of found adjacent vertex to form the actual path
+				adjVertex->setParentVertex(closestVertex);
+			}
+		}
+	}
+	
+	return std::vector<Vertex<T>*>(); // no path found
+
+
+}
+
+template<typename T>
+Vertex<T>* SearchAlgorithms<T>::getClosestVertex(std::vector<Vertex<T>*> verticesVector)
+{
+	Vertex<T>* closestVertex = verticesVector[0];
+
+	for (Vertex<T>* vertex : verticesVector)
+	{
+		//std::cout << std::to_string(vertex->getDistance()) << std::endl;
+		if (vertex->getDistance() < closestVertex->getDistance())
+		{
+			//std::cout << "TRUE!!!!!" << std::endl;
+			closestVertex = vertex;
+		}
+	}
+	return closestVertex;
+}
--- graphs_impl/SearchAlgorithms.h
+++ graphs_impl/SearchAlgorithms.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <queue>
 #include <stack>
+#include <list>
 #include "Graph.h"
 #include "Vertex.h"
 
@@ -18,9 +19,17 @@ public:
 	static std::vector<Vertex<T>*>* breadthFirstSearchWithGoal(Graph<T>* graph, Vertex<T>* sourceVertex, Vertex<T>* goalVertex);
 	static std::vector<Vertex<T>*>* getPathToSource(Vertex<T>* from);
 
-	//DFS
+	// DFS
 	static void depthFirstSearch(Graph<T>* graph, Vertex<T>* sourceVertex, bool reverseNeighbours = false);
 	static std::vector<Vertex<T>*>* depthFirstSearchWithGoal(Graph<T>* graph, Vertex<T>* sourceVertex, Vertex<T>* goalVertex, bool reverseNeighbours = false);
+
+	// Dijkstra
+	static void Dijkstra(Graph<T>* graph, Vertex<T>* sourceVertex);
+	static std::vector<Vertex<T>*> DijkstraWithGoal(Graph<T>* graph, Vertex<T>* sourceVertex, Vertex<T>* goalVertex);
+
+private:
+	// Dijkstra will use this method to find the closest vertex from current sourceVertex
+	static Vertex<T>* getClosestVertex(std::vector<Vertex<T>*> verticesVector);
 
 };
 
